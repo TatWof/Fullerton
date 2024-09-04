3181f21e30904365d7a49262f6b0e74350e80085
TatWof <104392674+TatWof@users.noreply.github.com>
2024-09-03 19:51:26 -0700

    linkage fix

--- 223C/assignment1/r.sh
+++ 223C/assignment1/r.sh
@@ -5,10 +5,12 @@ rm *.out
 
 gcc -c -Wall -m64 -no-pie *.c -o main.o -std=c17
 
-gcc -m64 -no-pie *.o -o main.out -std=c17
+gcc -m64 -no-pie *.o -lm -o main.out -std=c17
 
 echo "Program Start:"
 
 rm *.o
+
+chmod u+x main.out
 
 ./main.out
