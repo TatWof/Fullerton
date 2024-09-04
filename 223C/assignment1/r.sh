dbc5e575057bb0bebe4a65d42ee515a62f6ab717
TatWof <billytcleong@gmail.com>
2024-09-03 19:49:14 -0700

    linkage fix

--- 223C/assignment1/r.sh
+++ 223C/assignment1/r.sh
@@ -5,9 +5,11 @@ rm *.out
 
 gcc -c -Wall -m64 -no-pie *.c -o main.o -std=c17
 
-gcc -m64 -no-pie *.o -o main.out -std=c17
+gcc -m64 -no-pie *.o -lm -o main.out -std=c17
 
 echo "Program Start:"
+
+chmod u+x *.out
 
 rm *.o
 
