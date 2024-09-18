1efd98b63e56f5cf6e1170c287ccf9543b87b2b2
TatWof <104392674+TatWof@users.noreply.github.com>
2024-09-17 19:11:49 -0700

    ass2 startup

---
+++ 223C/assignment2/file2string.c
@@ -0,0 +1,17 @@
+/*  INFO
+    Name: Billy Leong
+    Email: bleong@csu.fullerton.edu
+    Course ID: CPSC223C
+    Due Date: 00/00/2024 - 23:59:00
+    Program Name:
+    Purpose:
+    OS: Windows 11
+*/
+
+#include "stdio.h"
+#include "string.h"
+
+void file2string(char* string, int stringSize, FILE* filename)
+{
+    
+}
--- 223C/assignment2/main.c
+++ 223C/assignment2/main.c
@@ -7,3 +7,28 @@
     Purpose:
     OS: Windows 11
 */
+
+#include "stdio.h"
+#include "string.h"
+
+extern file2string(char*, int, FILE*);
+void tokenize(char*);
+
+int main(int argc, char const *argv[])
+{
+    FILE* filename;
+    int stringSize;
+
+    fseek(filename, 0 ,SEEK_END);
+    stringSize = ftell(filename);
+    
+    char string[stringSize];
+
+    file2string(string, stringSize, filename);
+
+
+
+
+    return 0;
+}
+
